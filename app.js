8f9ee59a19426e7cbc0178fcdcef21acd89d7e9f
Amelia Roberts <arob0017@student.monash.edu>
2020-10-29 14:36:32 +1100

    add employee and add department working,

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cTable = require("console.table");
 const mysql = require("mysql");
 
 const connection = require('./db/connection');
+const { allowedNodeEnvironmentFlags } = require("process");
 
 connection.connect(function (err) {
     if (err) throw err;
@@ -17,7 +18,7 @@ async function init() {
                 type: 'list',
                 name: 'employeeChoice',
                 message: 'What would you like to do?',
-                choices: ['view employees', 'view departments', 'view roles'],
+                choices: ['view employees', 'view departments', 'view roles', 'add employee', 'add department', 'add role', 'update employee roles', 'quit'],
             },
         ]);
         console.info('Answer:', answers.employeeChoice);
@@ -25,21 +26,96 @@ async function init() {
             case "view employees":
                 const employeeRes = await connection.query("SELECT * FROM employee");
                 console.table(employeeRes);
+                init();
                 break;
             case "view departments":
                 const departmentRes = await connection.query("SELECT * FROM department");
                 console.table(departmentRes);
+                init();
                 break;
             case "view roles":
                 const roleRes = await connection.query("SELECT * FROM role");
                 console.table(roleRes);
-                break;
-            default:
                 init();
+                break;
+
+            case "add employee":
+                addEmployee();
+                break;
+            case "add department":
+                addDepartment();
+                break;
+            case "add role":
+                addRole();
+
+                break;
+            case "update employee roles":
+                updateEmpRole();
+                init();
+                break;
+
+            default:
+                console.log("GoodBye");
+                connection.end();
+                process.exit();
+
         }
 
     } catch (err) {
         console.log(err);
     }
+
+    function addEmployee() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter employee first name",
+                name: "firstname"
+            },
+            {
+                type: "input",
+                message: "Enter employee last name",
+                name: "lastname"
+            }
+        ]).then(function (answers) {
+            connection.query("INSERT INTO employee SET ?",
+                {
+                    first_name: answers.firstname,
+                    last_name: answers.lastname,
+                    role_id: null,
+                    manager_id: null
+                },
+                function (err, answers) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.table(answers);
+                }
+            );
+            init();
+        });
+    };
+    function addDepartment() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter new Department name:",
+                name: "newDepartment"
+            }
+        ]).then(function (answers) {
+            connection.query("INSERT INTO department SET ?",
+                {
+                    name: answers.newDepartment
+                },
+                function (err, answers) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.table(answers);
+                }
+            );
+            init();
+        });
+    };
 
 }
